7570f0a488bd2f4409358492559ddcfd3b4c2988
Vsevolod Konyakhin <sevakonyakhin@gmail.com>
2021-08-13 00:56:12 +0300

    added weights loading from PL checkpoint

---
+++ traffic4cast/util/lightning_util.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+from collections import OrderedDict
+
+
+def load_state_dict_from_lightning_checkpoint_(model: nn.Module, path: str, ):
+    checkpoint = torch.load(path, map_location=torch.device('cpu'))
+    lightning_state_dict = checkpoint['state_dict']
+
+    try:
+        model.load_state_dict(lightning_state_dict)
+    except RuntimeError as e:
+        print(e)
+        print("    Atttempting loading with keys renaming: ")
+
+        new_state_dict = OrderedDict()
+
+        for key in lightning_state_dict.keys():
+            sep_index = key.find(".")
+            new_state_dict[key[sep_index + 1:]] = lightning_state_dict[key]
+
+        model.load_state_dict(new_state_dict)
+
+        print("    Success! ")
