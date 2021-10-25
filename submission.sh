4885343569b6d924ae7027b9bdd695bea4cc323c
Seva Konyakhin <sevakonyakhin@gmail.com>
2021-10-25 23:47:39 +0300

    Update submission.sh

--- submission.sh
+++ submission.sh
@@ -37,7 +37,7 @@ python3 predict_to_file.py \
 # CHICAGO
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city CHICAGO \
   --model densenet_unet \
   --model_path weights/densenet/CHICAGO_1010_1730_densenet_unet_mse_best_val_loss_2019=41.1579.pth \
   --output_folder predictions \
@@ -46,7 +46,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city CHICAGO \
   --model unet \
   --model_path weights/unet/CHICAGO_0805_0038_vanilla_unet_mse_best_val_loss_2019=42.6634.pth \
   --output_folder predictions \
@@ -55,7 +55,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city CHICAGO \
   --model effnetb5_unet \
   --model_path weights/effnetb5/CHICAGO_1012_1035_efficientnetb5_unet_mse_best_val_loss_2019=41.6425.pth \
   --output_folder predictions \
@@ -65,7 +65,7 @@ python3 predict_to_file.py \
 # ISTANBUL
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city ISTANBUL \
   --model unet \
   --model_path weights/unet/ISTANBUL_0805_2317_vanilla_unet_mse_best_val_loss_2019=0.0000_v4.pth \
   --output_folder predictions \
@@ -74,7 +74,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city ISTANBUL \
   --model effnetb5_unet \
   --model_path weights/effnetb5/ISTANBUL_1012_2315_efficientnetb5_unet_mse_best_val_loss_2019=55.7918.pth \
   --output_folder predictions \
@@ -85,7 +85,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city MELBOURNE \
   --model densenet_unet \
   --model_path weights/densenet/MELBOURNE_1009_1619_densenet_unet_mse_best_val_loss_2019=25.7395.pth \
   --output_folder predictions \
@@ -94,7 +94,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city MELBOURNE \
   --model unet \
   --model_path weights/unet/MELBOURNE_0804_1942_vanilla_unet_mse_best_val_loss_2019=26.7588.pth \
   --output_folder predictions \
@@ -103,7 +103,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city MELBOURNE \
   --model effnetb5_unet \
   --model_path weights/effnetb5/MELBOURNE_1010_0058_efficientnetb5_unet_mse_best_val_loss_2019=26.0132.pth \
   --output_folder predictions \
@@ -142,7 +142,7 @@ python3 predict_to_file.py \
 # CHICAGO
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city CHICAGO \
   --model densenet_unet \
   --model_path weights/densenet/CHICAGO_1010_1730_densenet_unet_mse_best_val_loss_2019=41.1579.pth \
   --output_folder predictions \
@@ -151,7 +151,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city CHICAGO \
   --model unet \
   --model_path weights/unet/CHICAGO_0805_0038_vanilla_unet_mse_best_val_loss_2019=42.6634.pth \
   --output_folder predictions \
@@ -160,7 +160,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city CHICAGO \
   --model effnetb5_unet \
   --model_path weights/effnetb5/CHICAGO_1012_1035_efficientnetb5_unet_mse_best_val_loss_2019=41.6425.pth \
   --output_folder predictions \
@@ -170,7 +170,7 @@ python3 predict_to_file.py \
 # ISTANBUL
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city ISTANBUL \
   --model unet \
   --model_path weights/unet/ISTANBUL_0805_2317_vanilla_unet_mse_best_val_loss_2019=0.0000_v4.pth \
   --output_folder predictions \
@@ -179,7 +179,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city ISTANBUL \
   --model effnetb5_unet \
   --model_path weights/effnetb5/ISTANBUL_1012_2315_efficientnetb5_unet_mse_best_val_loss_2019=55.7918.pth \
   --output_folder predictions \
@@ -190,7 +190,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city MELBOURNE \
   --model densenet_unet \
   --model_path weights/densenet/MELBOURNE_1009_1619_densenet_unet_mse_best_val_loss_2019=25.7395.pth \
   --output_folder predictions \
@@ -199,7 +199,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city MELBOURNE \
   --model unet \
   --model_path weights/unet/MELBOURNE_0804_1942_vanilla_unet_mse_best_val_loss_2019=26.7588.pth \
   --output_folder predictions \
@@ -208,7 +208,7 @@ python3 predict_to_file.py \
 
 python3 predict_to_file.py \
   --dataset_path "$1" \
-  --city BERLIN \
+  --city MELBOURNE \
   --model effnetb5_unet \
   --model_path weights/effnetb5/MELBOURNE_1010_0058_efficientnetb5_unet_mse_best_val_loss_2019=26.0132.pth \
   --output_folder predictions \
@@ -217,4 +217,4 @@ python3 predict_to_file.py \
 
 
 # Generate the submission file based on ensembling the predictions from above:
-python3 scripts/generate_submission_from_files.py --dataset_path "$1"
+PYTHONPATH="$PWD" python3 scripts/generate_submission_from_files.py --dataset_path "$1"
